19b065b999306f37861dbf5b84a1e25dd81be2db
musamusakannike <musamusakannike@gmail.com>
2025-06-11 17:13:10 +0100

    Updated puppeteer browser launch options

--- utils/browserManager.js
+++ utils/browserManager.js
@@ -53,9 +53,12 @@ class BrowserManager {
       console.log("Initializing new browser instance");
       const browser = await puppeteer.launch({
         headless: "new",
+        executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH || null : puppeteer.executablePath(),
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
+          "--single-process",
+          "--no-zygote",
           "--disable-dev-shm-usage",
           "--disable-accelerated-2d-canvas",
           "--disable-gpu",
